39161dcf20f2f2d92482fb7bf67629166369d58a
Артём <artemka18041999@mail.ru>
2023-01-26 12:34:17 +0300

    1

--- bot2.py
+++ bot2.py
@@ -3,15 +3,15 @@ import requests
 from bs4 import BeautifulSoup
 
 user_id = 12345
-response = requests.get("https://www.kinoafisha.info/rating/movies/")
-soup = BeautifulSoup(response.content, "html.parser")
+film1 = requests.get("https://www.kinoafisha.info/rating/movies/")
+soup1 = BeautifulSoup(film1.content, "html.parser")
 
 merger = pd.DataFrame()
 #####
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 test_df = pd.DataFrame(columns=["name", "cla", "years", "country"])
-films = soup.find_all('div', class_="movieList_item")
+films = soup1.find_all('div', class_="movieList_item")
 counter = 0
 for film in films:
     title = film.find('a', class_="movieItem_title").text.strip()
@@ -25,16 +25,15 @@ for film in films:
     test_df.loc[counter, "country"] = x[1]
     counter += 1
 
-user_id = 12345
-response = requests.get("https://www.kinoafisha.info/rating/movies/?page=1")
-soup = BeautifulSoup(response.content, "html.parser")
+film2 = requests.get("https://www.kinoafisha.info/rating/movies/?page=1")
+soup2 = BeautifulSoup(film2.content, "html.parser")
 
 merger = pd.DataFrame()
 #####
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 test_df = pd.DataFrame(columns=["name", "cla", "years", "country"])
-films = soup.find_all('div', class_="movieList_item")
+films = soup2.find_all('div', class_="movieList_item")
 for film in films:
     title = film.find('a', class_="movieItem_title").text.strip()
     cla = film.find('span', class_="movieItem_genres").text.strip()
@@ -49,6 +48,15 @@ for film in films:
 #print(test_df)
 #print(test_df.sample())
 
+anime3 = requests.get("https://yummyanime.tv/1top-100/")
+soup3 =BeautifulSoup(anime3.content, "html.parser")
+
+dp_anime = pd.DataFrame()
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+test_df=pd.DataFrame(columns=["name", "cla", "years", "country"])
+
+
 from aiogram import Bot, Dispatcher, executor,types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 
@@ -60,9 +68,9 @@ dp=Dispatcher(bot)
 button_hi = KeyboardButton('привет')
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_hi)
 
-button_game1=KeyboardButton('давай в камень, ножницы тупо зарубим')
-button_game2=KeyboardButton('не, ну можно и в угадай число')
-button_NO=KeyboardButton('да чет влом сорри')
+button_game1=KeyboardButton('мултик')
+button_game2=KeyboardButton('сирик')
+button_NO=KeyboardButton('анумэ')
 button_film=KeyboardButton('давай фильм')
 greet3=ReplyKeyboardMarkup().add(button_game1).add(button_game2).add(button_film).add(button_NO)
 
@@ -78,20 +86,7 @@ async def get_text_messages(msg: types.Message):
         sam_film = '\n'.join(sam_film.to_string(index=False).split('\n')[1:])
         await msg.answer(f'{sam_film}')
     if msg.text.lower() == 'не, ну можно и в угадай число':
-        import random
-        RandomnoeChislo = random.randint(1, 100)
-        NasheChislo = -1
-        while NasheChislo != RandomnoeChislo:
-            @dp.message_handler()
-            async def echo_message(msg: types.Message):
-                NasheChislo = msg.text
-                NasheChislo = bot.send_message(message.chat.id, 'Угадайте число от 1 до 100')
-                if NasheChislo > RandomnoeChislo:
-                    await NasheChislo.text("Число должно быть меньше вонючка")
-                elif NasheChislo < RandomnoeChislo:
-                    await NasheChislo.text(("Число должно быть больше козел"))
-                else:
-                    await NasheChislo.text("Ооооооо яяяяяя дастишь фантастишь")
+        await msg.answer('думаю как сделать')
     if msg.text.lower() == 'давай в камень, ножницы тупо зарубим':
         await msg.answer('думаю как сделать')
     if msg.text.lower() == 'да чет влом сорри':
